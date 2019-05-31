a41a97739f8f79f8b2154b7ab2c0a71a1ae121d1
Eko Eryanto <ekoeryanto@gmail.com>
2019-06-01 00:01:03 +0700

    chore: add editlink and last updated

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,14 @@
 module.exports = {
   title: 'PeTi',
   description: 'Pembayaran Tagihan PDAM Tirta Tarum Kabupaten Karawang',
+  head: [
+    ['link', { rel: 'icon', href: '/peti.svg' }]
+  ],
   themeConfig: {
+    lastUpdated: true,
+    repo: 'tirtatarum/peti',
+    docsDir: 'docs',
+    editLinks: true,
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
